9747573b7ab12ceb1756faa44388f5f15f86fdfc
Mykhaylo Petrenko <107565609+MPetrenko22@users.noreply.github.com>
2023-06-30 16:39:12 +0300

    Delete 0_1_CleanBuffers.sql

--- prepackage/athena/sql/list_calculation/0_1_CleanBuffers.sql
+++
@@ -1,7 +0,0 @@
-/* 0.1 Clean Buffers After Errors */
-
-delete from lvmodel.m_pre_itbf_template_demands where list_id = ?;
-delete from lvmodel.m_pre_itbf_contact_new_collection where list_id = ?;
-delete from lvmodel.m_pre_itbf_campaign_requirement_options where list_id = ?;
-delete from lvmodel.m_pre_itbf_campaign_demand_mask where list_id = ?;
-delete from lvmodel.m_pre_itbf_approved_new_collection where list_id = ?;
