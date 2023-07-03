85bf5844b442c941819bf445fccefa23e8486552
Mykhaylo Petrenko <107565609+MPetrenko22@users.noreply.github.com>
2023-07-03 16:46:24 +0300

    Add files via upload

---
+++ prepackage/athena/sql/stage/calculate_prepackage/999_processeTimedOut.sql
@@ -0,0 +1,2 @@
+update lvmodel_stage.m_pre_itbf_new_list_to_check set status = -1,  prepackage_contact_count = 0, processed_at = NOW() where status = 0
+;
---
+++ prepackage/athena/sql/stage/calculate_prepackage/9_CleanWorkingTables.sql
@@ -0,0 +1,7 @@
+/* 9 Clean working tables where calculations have finished*/
+
+delete from lvmodel_stage.m_pre_itbf_template_demands where list_id = ?;
+delete from lvmodel_stage.m_pre_itbf_contact_new_collection where list_id = ?;
+delete from lvmodel_stage.m_pre_itbf_campaign_requirement_options where list_id = ?;
+delete from lvmodel_stage.m_pre_itbf_campaign_demand_mask where list_id = ?;
+delete from lvmodel_stage.m_pre_itbf_approved_new_collection where list_id = ?;
