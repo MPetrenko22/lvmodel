9f2f69b654b7ba46948b507beab7b5a473b622b6
Mykhaylo Petrenko <107565609+MPetrenko22@users.noreply.github.com>
2023-07-03 16:33:08 +0300

    Delete prepackage/athena/sql/list_calculation directory

--- prepackage/athena/sql/list_calculation/1.2CleanPreviousRecords.sql
+++
@@ -1,8 +0,0 @@
-/* 1.2 Clean previous records of new list id from Result Table (if new list id was calculated before)*/
-
-merge into lvmodel.m_pre_itbf_final t 
-using
-	(select distinct list_id from lvmodel.m_pre_itbf_new_list_to_check where list_id = ?) s
-on (t.list_id = s.list_id)
-when matched then delete;
-
--- prepackage/athena/sql/list_calculation/999_processeTimedOut.sql
+++
@@ -1,2 +0,0 @@
-update lvmodel.m_pre_itbf_new_list_to_check set status = -1,  prepackage_contact_count = 0, processed_at = NOW() where status = 0
-;
