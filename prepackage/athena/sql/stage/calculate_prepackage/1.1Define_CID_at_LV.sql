f098d07082680318851a991a862ad3c8fc57446b
Mykhaylo Petrenko <107565609+MPetrenko22@users.noreply.github.com>
2023-07-06 16:01:19 +0300

    Update 1.1Define_CID_at_LV.sql

--- prepackage/athena/sql/stage/calculate_prepackage/1.1Define_CID_at_LV.sql
+++ prepackage/athena/sql/stage/calculate_prepackage/1.1Define_CID_at_LV.sql
@@ -13,8 +13,8 @@ using
 	(
 		select 
 			ca.cid, l.id as list_id 
-		from "lv-prepackage-stage".app_lv.lists l 
-		inner join "lv-prepackage-stage".app_lv.campaigns ca on ca.id = l.campaign_id and ca.type = 2
+		from "lv-prepackage-stage".lv_stage_hotfix.lists l 
+		inner join "lv-prepackage-stage".lv_stage_hotfix.campaigns ca on ca.id = l.campaign_id and ca.type = 2
 		where l.id = ?
 	) s
 on (t.list_id = s.list_id and t.status is null)
@@ -24,5 +24,3 @@ when matched then update set cid = s.cid, status = 0, id = (select max(case when
 update lvmodel_stage.m_pre_itbf_new_list_to_check
 set status = -1, id = (select max(case when c2.id is null then 0 else c2.id end) from lvmodel_stage.m_pre_itbf_new_list_to_check c2) + 1 
 where status is null and list_id = ?;
-
-
