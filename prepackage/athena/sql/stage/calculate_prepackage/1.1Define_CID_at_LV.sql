0494a863e35bb3d512665fe004ca7737b7963fca
Mykhaylo Petrenko <107565609+MPetrenko22@users.noreply.github.com>
2023-07-06 16:35:49 +0300

    Update 1.1Define_CID_at_LV.sql

--- prepackage/athena/sql/stage/calculate_prepackage/1.1Define_CID_at_LV.sql
+++ prepackage/athena/sql/stage/calculate_prepackage/1.1Define_CID_at_LV.sql
@@ -1,7 +1,8 @@
 /* 1.1 Define CID at LV */
 
 /* Delete list_id dubles from previous calculation - to avoid errors  */
-delete from lvmodel_stage.m_pre_itbf_new_list_to_check where list_id = ? and id is not null;
+delete from lvmodel_stage.m_pre_itbf_new_list_to_check where list_id = ? and id is not null
+;
 
 
 
@@ -18,9 +19,11 @@ using
 		where l.id = ?
 	) s
 on (t.list_id = s.list_id and t.status is null)
-when matched then update set cid = s.cid, status = 0, id = (select max(case when id is null then 0 else id end) from lvmodel_stage.m_pre_itbf_new_list_to_check) + 1;
+when matched then update set cid = s.cid, status = 0, id = (select max(case when id is null then 0 else id end) from lvmodel_stage.m_pre_itbf_new_list_to_check) + 1
+;
 
 
 update lvmodel_stage.m_pre_itbf_new_list_to_check
 set status = -1, id = (select max(case when c2.id is null then 0 else c2.id end) from lvmodel_stage.m_pre_itbf_new_list_to_check c2) + 1 
-where status is null and list_id = ?;
+where status is null and list_id = ?
+;
