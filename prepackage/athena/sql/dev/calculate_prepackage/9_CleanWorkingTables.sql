9b78c88249921918642f5335d0bf2079a3f29582
Mykhaylo Petrenko <107565609+MPetrenko22@users.noreply.github.com>
2023-07-10 15:04:16 +0300

    Update 9_CleanWorkingTables.sql

--- prepackage/athena/sql/dev/calculate_prepackage/9_CleanWorkingTables.sql
+++ prepackage/athena/sql/dev/calculate_prepackage/9_CleanWorkingTables.sql
@@ -1,12 +1,12 @@
 /* 9 Clean working tables where calculations have finished*/
 
-delete from lv_athena_stage.m_pre_itbf_template_demands where list_id = ?
+delete from lvmodel_dev.m_pre_itbf_template_demands where list_id = ?
 ;
-delete from lv_athena_stage.m_pre_itbf_contact_new_collection where list_id = ?
+delete from lvmodel_dev.m_pre_itbf_contact_new_collection where list_id = ?
 ;
-delete from lv_athena_stage.m_pre_itbf_campaign_requirement_options where list_id = ?
+delete from lvmodel_dev.m_pre_itbf_campaign_requirement_options where list_id = ?
 ;
-delete from lv_athena_stage.m_pre_itbf_campaign_demand_mask where list_id = ?
+delete from lvmodel_dev.m_pre_itbf_campaign_demand_mask where list_id = ?
 ;
-delete from lv_athena_stage.m_pre_itbf_approved_new_collection where list_id = ?
+delete from lvmodel_dev.m_pre_itbf_approved_new_collection where list_id = ?
 ;
