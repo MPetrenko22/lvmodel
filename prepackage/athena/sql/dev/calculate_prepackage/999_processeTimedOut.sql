c5aed9307e962716435f16fa561d7775d4eee3d4
Mykhaylo Petrenko <107565609+MPetrenko22@users.noreply.github.com>
2023-07-10 15:03:54 +0300

    Update 999_processeTimedOut.sql

--- prepackage/athena/sql/dev/calculate_prepackage/999_processeTimedOut.sql
+++ prepackage/athena/sql/dev/calculate_prepackage/999_processeTimedOut.sql
@@ -1,2 +1,2 @@
-update lv_athena_stage.m_pre_itbf_new_list_to_check set status = -1,  prepackage_contact_count = 0, processed_at = NOW() where status = 0
+update lvmodel_dev.m_pre_itbf_new_list_to_check set status = -1,  prepackage_contact_count = 0, processed_at = NOW() where status = 0
 ;
