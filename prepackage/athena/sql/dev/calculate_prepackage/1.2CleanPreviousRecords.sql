26b291e250c0c7cac9ce78a6976f06dce4d9c061
Mykhaylo Petrenko <107565609+MPetrenko22@users.noreply.github.com>
2023-07-10 14:19:13 +0300

    Update 1.2CleanPreviousRecords.sql

--- prepackage/athena/sql/dev/calculate_prepackage/1.2CleanPreviousRecords.sql
+++ prepackage/athena/sql/dev/calculate_prepackage/1.2CleanPreviousRecords.sql
@@ -1,9 +1,8 @@
 /* 1.2 Clean previous records of new list id from Result Table (if new list id was calculated before)*/
 
-merge into lvmodel_stage.m_pre_itbf_final t 
+merge into lv_athena_stage.m_pre_itbf_final t 
 using
-	(select distinct list_id from lvmodel_stage.m_pre_itbf_new_list_to_check where list_id = ?) s
+	(select distinct list_id from lv_athena_stage.m_pre_itbf_new_list_to_check where list_id = ?) s
 on (t.list_id = s.list_id)
 when matched then delete
 ;
-
