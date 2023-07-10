64104d1b931339799e14bc20af21679f7c24fffd
Mykhaylo Petrenko <107565609+MPetrenko22@users.noreply.github.com>
2023-07-10 14:15:57 +0300

    Add files via upload

---
+++ prepackage/athena/sql/dev/calculate_prepackage/1.3NewCollectionatLV.sql
@@ -0,0 +1,71 @@
+/* 1.3 New Collection at LV*/
+/* Import all contacts and companies from LV belong to the new list id */
+
+insert into lvmodel_stage.m_pre_itbf_contact_new_collection
+(
+	cid,
+	"type",
+	campaign_id,
+	list_id,
+	session_id,
+	email_id,
+	email,
+	id,
+	contact_id,
+	ext_contact_id,
+	title,
+	country,
+	state,
+	contact_phone,
+	mobile_phone,
+	pv_comment,
+	company_id,
+	ext_company_id,
+	employee,
+	industry,
+	company_phone,
+	status
+)
+SELECT distinct
+	ca.cid
+	, ca.type
+	, c.campaign_id
+	, c.list_id
+	, 0 as session_id
+	, c.email_id
+	, c.email
+	, c.id
+	, c.id AS contact_id
+	, cast(c.contact_id as bigint) AS ext_contact_id
+	, ct.title
+	, co.short_name AS country
+	, c.state
+	, case when cph.phone in ('','DNC') then null else cph.phone end as contact_phone
+	, case when cmp.mobile_phone in ('','DNC') then null else cmp.mobile_phone end mobile_phone
+	, pr.key_name as pv_comment
+	, com.id AS company_id
+	, com.company_id AS ext_company_id
+	, CONCAT(cast(r."min" AS varchar(255)), ';', cast(r."max" AS varchar(255))) AS employee
+	, CONCAT(i.name, ';', i2.name) AS industry
+	, case when comph.phone in ('','DNC') then null else comph.phone end as company_phone
+	, 0
+FROM "lv-prepackage-stage".lv_stage_hotfix.campaigns ca
+INNER JOIN "lv-prepackage-stage".lv_stage_hotfix.contacts c ON c.campaign_id = ca.id
+INNER JOIN "lv-prepackage-stage".lv_stage_hotfix.contact_titles ct ON ct.contact_id = c.id
+INNER JOIN "lv-prepackage-stage".lv_stage_hotfix.lists l ON l.id = c.list_id and l.local_type = 0
+INNER JOIN "lv-prepackage-stage".lv_stage_hotfix.states s ON s.abbr = c.state AND s.state_id > 0
+INNER JOIN "lv-prepackage-stage".lv_stage_hotfix.contact_countries sc on sc.contact_id = c.id
+INNER JOIN "lv-prepackage-stage".lv_stage_hotfix.countries co ON co.id = sc.country_id
+INNER JOIN "lv-prepackage-stage".lv_stage_hotfix.companies com ON com.id = c.company_id
+INNER JOIN "lv-prepackage-stage".lv_stage_hotfix.company_ranges r ON r.ref_id = com.employees_ref AND r."type" = 'employee' 
+INNER JOIN "lv-prepackage-stage".lv_stage_hotfix.company_industries ci ON ci.ref_id = com.industry_ref
+INNER JOIN "lv-prepackage-stage".lv_stage_hotfix.industries i ON i.id = ci.industry_id
+INNER JOIN "lv-prepackage-stage".lv_stage_hotfix.industries i2 ON i2.id = ci.sub_industry_id
+inner join "lv-prepackage-stage".lv_stage_hotfix.phone_reasons pr on pr.id = c.phone_reason_id
+left join "lv-prepackage-stage".lv_stage_hotfix.contact_phones cph on cph.contact_id = c.id
+left join "lv-prepackage-stage".lv_stage_hotfix.contact_mobile_phones cmp on cmp.contact_id = c.id
+left join "lv-prepackage-stage".lv_stage_hotfix.company_phones comph on comph.ref_id = com.phone_ref
+where 
+	r.min < 1000000000 and r.max < 1000000000 /*to avoid int out of range error*/
+	and l.id = ?
+;
---
+++ prepackage/athena/sql/dev/calculate_prepackage/4_CIDRequirementOption.sql
@@ -0,0 +1,56 @@
+/* 4 CID Requirement Option*/
+/* Import the verification requirements for the campaign */
+
+
+insert into lvmodel_stage.m_pre_itbf_campaign_requirement_options
+(
+	id, 
+	campaign_id, 
+	cid,
+	list_id,
+	session_id,
+	is_latest, 
+	employees_empty, 
+	employees_skip, 
+	geo_empty, 
+	geo_skip, 
+	industry_skip,
+	status
+)
+SELECT  DISTINCT
+	q.id, q.campaign_id, j.cid, j.list_id, j.id as session_id, q.is_latest, q.employees_empty, q.employees_skip, q.geo_empty, q.geo_skip, q.industry_skip, 0 
+FROM "lv-prepackage-stage".lv_stage_hotfix.campaign_requirement_options q
+INNER JOIN "lv-prepackage-stage".lv_stage_hotfix.lists l on l.campaign_id = q.campaign_id
+INNER JOIN lvmodel_stage.m_pre_itbf_new_list_to_check j on j.list_id = l.id and j.status = 0
+where q.is_latest = cast(1 as boolean)
+	and j.list_id = ?
+;
+
+
+
+
+/* Skip the employee, geo and industry demands when campaign verification requirements have their skip*/
+merge into lvmodel_stage.m_pre_itbf_campaign_demand_mask t 
+using
+	(
+		select distinct
+			o.cid,
+			o.list_id,
+			case when o.employees_empty = try_cast(1 as boolean) or o.employees_skip = try_cast(1 as boolean) then 0 else null end skip_employee,
+			case when o.geo_empty = try_cast(1 as boolean) or o.geo_skip = try_cast(1 as boolean) then 0 else null end skip_geo,
+			case when o.industry_skip = try_cast(1 as boolean) then 0 else null end skip_industry
+		from lvmodel_stage.m_pre_itbf_campaign_requirement_options o
+		where o.list_id = ?
+	) s
+on (t.list_id = s.list_id)
+when matched then update set 
+							country = if(s.skip_geo = 0, 0, country),
+							a_country = if(s.skip_geo = 0, 0, a_country),
+							state = if(s.skip_geo = 0, 0, state),
+							a_state = if(s.skip_geo = 0, 0, a_state),
+							employee = if(s.skip_employee = 0, 0, employee),
+							a_employee = if(s.skip_employee = 0, 0, a_employee),
+							industry = if(s.skip_industry = 0, 0, industry),
+							a_industry = if(s.skip_industry = 0, 0, a_industry),
+							status = 2
+;
