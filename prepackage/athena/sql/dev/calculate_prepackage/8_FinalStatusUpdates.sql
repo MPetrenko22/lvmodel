3b4be391f02797f29651635b58b49699cbcb3e3d
Mykhaylo Petrenko <107565609+MPetrenko22@users.noreply.github.com>
2023-07-10 15:03:29 +0300

    Update 8_FinalStatusUpdates.sql

--- prepackage/athena/sql/dev/calculate_prepackage/8_FinalStatusUpdates.sql
+++ prepackage/athena/sql/dev/calculate_prepackage/8_FinalStatusUpdates.sql
@@ -1,12 +1,12 @@
 /* 8 Final Status Updates */
 /* Update statuses when calculation has been finished*/
 
-merge into lv_athena_stage.m_pre_itbf_new_list_to_check t 
+merge into lvmodel_dev.m_pre_itbf_new_list_to_check t 
 using
 	(
 		select c.list_id, count(distinct f.contact_id) cnt 
-		from lv_athena_stage.m_pre_itbf_new_list_to_check c
-		left join lv_athena_stage.m_pre_itbf_final f on f.list_id = c.list_id 
+		from lvmodel_dev.m_pre_itbf_new_list_to_check c
+		left join lvmodel_dev.m_pre_itbf_final f on f.list_id = c.list_id 
 		where c.list_id = ? 
 		group by c.list_id
 	) s
