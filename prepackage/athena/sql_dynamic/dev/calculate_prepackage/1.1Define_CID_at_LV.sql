941c734e0eb81e077b8d7674ffeddfdae03db574
Mykhaylo Petrenko <107565609+MPetrenko22@users.noreply.github.com>
2023-07-14 23:06:44 +0300

    Update 1.1Define_CID_at_LV.sql

--- prepackage/athena/sql_dynamic/dev/calculate_prepackage/1.1Define_CID_at_LV.sql
+++ prepackage/athena/sql_dynamic/dev/calculate_prepackage/1.1Define_CID_at_LV.sql
@@ -1,14 +1,22 @@
 /* 1.1 Define CID at LV */
 /*Create tmp table*/
 
-CREATE TABLE lvmodel_dev.m_pre_itbf_new_list_to_check_? 
+CREATE TABLE lvmodel_dev.m_pre_itbf_new_list_to_check_? (
+	list_id int,
+	cid string,
+	status int
+)
 LOCATION 's3://lvprepackage/dev/iceberg/'
 TBLPROPERTIES (
   'table_type'='iceberg',
   'format'='parquet'
 )
-AS SELECT 
-ca.cid, l.id AS list_id, 0 AS status
+;
+
+
+INSERT INTO lvmodel_dev.m_pre_itbf_new_list_to_check_?
+(cid, list_id, status)
+SELECT ca.cid, l.id AS list_id, 0 AS status
 FROM "lv-prepackage-stage".lv_athena_stage.lists l 
 INNER JOIN "lv-prepackage-stage".lv_athena_stage.campaigns ca on ca.id = l.campaign_id and ca.type = 2
 WHERE l.id = ?
