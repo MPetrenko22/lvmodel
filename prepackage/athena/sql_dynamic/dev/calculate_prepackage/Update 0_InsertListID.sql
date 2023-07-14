3fec48b6d068634027626f2d8def8a1ba4be0396
Mykhaylo Petrenko <107565609+MPetrenko22@users.noreply.github.com>
2023-07-14 22:11:11 +0300

    Update Update 0_InsertListID.sql

--- prepackage/athena/sql_dynamic/dev/calculate_prepackage/Update 0_InsertListID.sql
+++ prepackage/athena/sql_dynamic/dev/calculate_prepackage/Update 0_InsertListID.sql
@@ -1,4 +1,4 @@
 /* 0 Insert List ID */ 
 /* Insert list id into Status Table for the further algorithm */
-insert into lvmodel_dev.m_pre_itbf_new_list_to_check(list_id, created_at) values (?, NOW())
+insert into lvmodel_dev.m_pre_itbf_new_list_to_check(list_id, status , created_at) values (?, 0, NOW())
 ;
