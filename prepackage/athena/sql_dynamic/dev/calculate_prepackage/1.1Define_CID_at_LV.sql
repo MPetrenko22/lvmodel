1a076f1a6507d82ba4e960e1ab55a68fe9997e4a
Mykhaylo Petrenko <107565609+MPetrenko22@users.noreply.github.com>
2023-07-16 15:08:27 +0300

    Update 1.1Define_CID_at_LV.sql

--- prepackage/athena/sql_dynamic/dev/calculate_prepackage/1.1Define_CID_at_LV.sql
+++ prepackage/athena/sql_dynamic/dev/calculate_prepackage/1.1Define_CID_at_LV.sql
@@ -23,7 +23,7 @@ WHERE l.id = ?
 ;
 
 
-INSERT INTO lvmodel_dev.m_pre_itbf_new_list_to_check (list_id, status, created_at)
-SELECT list_id, CASE WHEN COUNT(*) > 0 THEN 0 ELSE -1 END, NOW() FROM lvmodel_dev.m_pre_itbf_new_list_to_check_?
-GROUP BY list_id
+INSERT INTO lvmodel_dev.m_pre_itbf_new_list_to_check (cid, list_id, status, created_at)
+SELECT cid, list_id, CASE WHEN COUNT(*) > 0 THEN 0 ELSE -1 END, NOW() FROM lvmodel_dev.m_pre_itbf_new_list_to_check_?
+GROUP BY cid, list_id
 ;
