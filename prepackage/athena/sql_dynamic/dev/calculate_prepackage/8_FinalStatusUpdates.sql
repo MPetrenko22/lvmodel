39128f7819d72f447bc7d1e81f145a032050a141
Mykhaylo Petrenko <107565609+MPetrenko22@users.noreply.github.com>
2023-07-16 16:22:21 +0300

    Update 8_FinalStatusUpdates.sql

--- prepackage/athena/sql_dynamic/dev/calculate_prepackage/8_FinalStatusUpdates.sql
+++ prepackage/athena/sql_dynamic/dev/calculate_prepackage/8_FinalStatusUpdates.sql
@@ -8,14 +8,14 @@ SELECT * FROM lvmodel_dev.m_pre_itbf_final_?
 
 
 /* Update statuses when calculation has been finished*/
-INSERT INTO lvmodel_dev.m_pre_itbf_new_list_to_check(list_id, status, prepackage_contact_count, created_at, processed_at)
+INSERT INTO lvmodel_dev.m_pre_itbf_new_list_to_check(cid, list_id, status, prepackage_contact_count, created_at, processed_at)
 WITH ST AS
 (
-	SELECT f.list_id, 1 AS status, COUNT(*) AS prepackage_contact_count, NOW() AS created_at, NOW() AS processed_at
+	SELECT f.cid, f.list_id, 1 AS status, COUNT(*) AS prepackage_contact_count, NOW() AS created_at, NOW() AS processed_at
 	FROM lvmodel_dev.m_pre_itbf_final_? f
-	GROUP BY f.list_id
+	GROUP BY f.cid, f.list_id
 )
-SELECT list_id, status, prepackage_contact_count, created_at, processed_at
+SELECT cid, list_id, status, prepackage_contact_count, created_at, processed_at
 FROM ST
 ;
 
