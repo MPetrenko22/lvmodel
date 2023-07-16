5d1bd242be707e222b0da72eda3d6ca85cf61b69
Mykhaylo Petrenko <107565609+MPetrenko22@users.noreply.github.com>
2023-07-16 16:03:23 +0300

    Update 999_processeTimedOut.sql

--- prepackage/athena/sql_dynamic/dev/calculate_prepackage/999_processeTimedOut.sql
+++ prepackage/athena/sql_dynamic/dev/calculate_prepackage/999_processeTimedOut.sql
@@ -1,2 +1,13 @@
-update lvmodel_dev.m_pre_itbf_new_list_to_check set status = -1,  prepackage_contact_count = 0, processed_at = NOW() where status = 0
+insert into lvmodel_dev.m_pre_itbf_new_list_to_check(list_id, status, created_at, processed_at)
+with MX as
+(
+	select c.list_id, MAX(created_at) created_at_max
+	from lvmodel_dev.m_pre_itbf_new_list_to_check c
+	where c.list_id = ?
+	group by c.list_id
+)
+select ch.list_id, -1 as status, NOW() AS created_at, NOW() AS processed_at
+from lvmodel_dev.m_pre_itbf_new_list_to_check ch
+inner join MX on MX.list_id = ch.list_id and ch.created_at = MX.created_at_max
+where ch.status = 0
 ;
