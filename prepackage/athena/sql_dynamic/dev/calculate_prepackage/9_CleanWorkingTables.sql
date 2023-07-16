749e93e25ff256a9bf26981a04c7cd6fc7d61320
Mykhaylo Petrenko <107565609+MPetrenko22@users.noreply.github.com>
2023-07-16 15:12:41 +0300

    Update 9_CleanWorkingTables.sql

--- prepackage/athena/sql_dynamic/dev/calculate_prepackage/9_CleanWorkingTables.sql
+++ prepackage/athena/sql_dynamic/dev/calculate_prepackage/9_CleanWorkingTables.sql
@@ -1,1 +1,22 @@
+/* 9 Clean working tables where calculations have finished*/
 
+DROP TABLE lvmodel_dev.m_pre_itbf_final_?
+;
+
+DROP TABLE lvmodel_dev.m_pre_itbf_template_demands_?
+;
+
+DROP TABLE lvmodel_dev.m_pre_itbf_contact_new_collection_?
+;
+
+DROP TABLE lvmodel_dev.m_pre_itbf_campaign_requirement_options_?
+;
+
+DROP TABLE lvmodel_dev.m_pre_itbf_campaign_demand_mask_?
+;
+
+DROP TABLE lvmodel_dev.m_pre_itbf_approved_new_collection_?
+;
+
+DROP TABLE lvmodel_dev.m_pre_itbf_contact_new_collection_?
+;
