9614cd138f48bb1072e4e59197704064c4513b67
Mykhaylo Petrenko <107565609+MPetrenko22@users.noreply.github.com>
2023-07-16 14:29:59 +0300

    Update 7_DefinePV.sql

--- prepackage/athena/sql_dynamic/dev/calculate_prepackage/7_DefinePV.sql
+++ prepackage/athena/sql_dynamic/dev/calculate_prepackage/7_DefinePV.sql
@@ -1,1 +1,56 @@
+/* 7 Define PV*/
+/* LC Statuses */
 
+merge into  lvmodel_dev.m_pre_itbf_final_? f
+using (
+	select  distinct
+		n.contact_id, pv.pv_comment
+	from lvmodel_dev.m_pre_itbf_contact_new_collection_? n 
+	inner join lvmodel_dev.m_pre_itbf_pv_history pv 
+		on pv.ext_contact_id = n.ext_contact_id 
+		and (n.contact_phone = pv.contact_phone or n.mobile_phone  = pv.mobile_phone)
+		and pv.pv_comment in ('lc1','lc2','lc3','lc4')
+		and n.pv_comment = 'n/a'
+	where n.list_id = ?
+) p
+on (p.contact_id = f.contact_id)
+when matched then update 
+	set pv_comment = p.pv_comment
+;
+
+
+
+/* CC Statuses */
+merge into  lvmodel_dev.m_pre_itbf_final_? f
+using (
+	select  distinct
+		n.contact_id, pv.pv_comment
+	from lvmodel_dev.m_pre_itbf_contact_new_collection_? n 
+	inner join lvmodel_dev.m_pre_itbf_pv_history pv 
+		on pv.ext_contact_id = n.ext_contact_id 
+		and n.company_phone = pv.company_phone
+		and pv.pv_comment in ('cc1','cc2','cc3','cc4')
+		and n.pv_comment = 'n/a'
+	where n.list_id = ?
+) p
+on (p.contact_id = f.contact_id)
+when matched then update 
+	set pv_comment = p.pv_comment
+;
+
+
+
+
+/* Prepackafe PV Code */
+update lvmodel_dev.m_pre_itbf_final_?
+set prepackage_code = 1
+where list_id = ?
+	and pv_comment in ('lc1','lc2','lc3','lc4','cc1','cc2','cc3','cc4')
+;
+
+
+
+/*Insert Final Result into common table*/
+INSERT INTO lvmodel_dev.m_pre_itbf_final
+SELECT * FROM lvmodel_dev.m_pre_itbf_final_?
+;
