8a16aceaeab5991dbcbdc8fc78130eb562b4eba9
Mykhaylo Petrenko <107565609+MPetrenko22@users.noreply.github.com>
2023-10-12 17:16:47 +0300

    Delete prepackage/athena/sql_dynamic directory

--- prepackage/athena/sql_dynamic/dev/calculate_prepackage/0_InsertListID.sql
+++
@@ -1,4 +0,0 @@
-/* 0 Insert List ID */ 
-/* Insert list id into Status Table for the further algorithm */
-insert into lvmodel_dev.m_pre_itbf_new_list_to_check(list_id, status , created_at) values (?, 0, NOW())
-;
